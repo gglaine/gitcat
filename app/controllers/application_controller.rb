a24c469cdfe018c91b77d6931b13f029cf491a93
Guillaume Laine <guillaumelaine2.0@gmail.com>
2018-01-12 02:10:45 +0100

    sign in and up, users controller, avatar, card, button components

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(user)
+    user = current_user
+    root_path
+  end
+
+  private
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email)
+    end
+  end
+  # Overwriting the sign_out redirect path method
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
